8b0d3cd89fdb61934041fd389338793d163f2d2a
Prevu-Bienheureux <prevumayitoukoustudent@gmail.com>
2025-06-20 08:17:47 +0100

    setting app appearance

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import customtkinter
 from customtkinter import CTk
+
 customtkinter.set_appearance_mode("system")
-customtkinter.set_default_color_theme("dark_blue")
+
 class App(CTk) : 
     def __init__(self):
         super().__init__()
         self.title("Salon App")
-        self.geometry(720,500)
+        self.geometry("720x500")
 
 
 App =App()
